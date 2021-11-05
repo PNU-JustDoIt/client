18eb0255714f9c2357513903b0437e46ccac3329
KIMBEOBWOO <qjqdn1568@naver.com>
2021-11-05 16:52:14 +0900

    Style: root View Tag style
    테스트를 위해 생성한 법우 page component 의 root view tag 에 style 을 추가함.

--- src/components/screens/beobwoo/BeobwooTestPage.tsx
+++ src/components/screens/beobwoo/BeobwooTestPage.tsx
@@ -3,7 +3,13 @@ import {View, Text} from 'react-native';
 
 export default function BeobwooTestPage(): JSX.Element {
   return (
-    <View>
+    <View
+      style={{
+        display: 'flex',
+        backgroundColor: 'tomato',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
       <Text>법우 페이지</Text>
     </View>
   );
