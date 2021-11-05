21b984f9d7259caf14b8ef4d449afae6910c0fe2
KIMBEOBWOO <qjqdn1568@naver.com>
2021-11-05 16:48:38 +0900

    Feat: 법우 테스트 Page Component 생성
    테스트를 위해 react native functional component 생성을 진행함.
    Props 는 없으며 Safearea view 가 아닌 View 가 root tag 이다.

---
+++ src/components/screens/beobwoo/BeobwooTestPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function BeobwooTestPage(): JSX.Element {
+  return (
+    <View>
+      <Text>법우 페이지</Text>
+    </View>
+  );
+}
